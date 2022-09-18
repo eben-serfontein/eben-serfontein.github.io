8ec2795b94e040abda71a0a77811d8cb948baaec
Eben Serfontein <ebenserf@gmail.com>
2022-09-19 05:46:06 +1200

    finish sprint 3 blog

---
+++ blog/test.js
@@ -0,0 +1,24 @@
+
+
+arrayContents = ['Ben', 'Martha', 'James']
+
+arrayContents[0]
+
+
+let objectContents = {
+  price: 89.90,
+  buyer: 'Maggie'
+  manager: 'Arthur'
+}
+objectContents.price
+objectContents['buyer']
+
+
+function functionName() {
+  if ()
+  for ()
+  console.log()
+  return()
+}
+
+functionName()
